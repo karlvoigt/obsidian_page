8650dfe81406ccaa3265f452137016215b450952
kvoigt <24867209@sun.ac.za>
2026-05-27 10:42:57 +0200

    Fix sidebar buttons

--- components/misc/header.tsx
+++ components/misc/header.tsx
@@ -15,8 +15,8 @@ const Header = ({ hasSidebars }: { hasSidebars?: boolean }) => {
   }, [top]);  
 
   return (
-    <header className={`fixed w-full z-30 md:bg-white/90 transition duration-300 ease-in-out ${!top && 'bg-white backdrop-blur-sm shadow-lg'}`}>
-      <div className="max-w-6xl mx-auto px-5 sm:px-6">
+    <header className={`fixed w-full z-50 md:bg-white/90 transition duration-300 ease-in-out ${!top && 'bg-white backdrop-blur-sm shadow-lg'}`}>
+      <div className="w-full px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 md:h-20">
           <div className="flex items-center">
             {hasSidebars && (
